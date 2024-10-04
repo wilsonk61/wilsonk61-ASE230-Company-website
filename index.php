4a3090f179997e9e30004d936f69aab1cf6c8500
JacksonWiseBusiness <jacksonwise.business@gmail.com>
2024-10-04 15:41:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-
+<?php   
+require_once 'lib/json_read_function.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -133,135 +135,49 @@
     <!--SERVICES BELOW-- JSON FILE -->
 
     <!--START SERVICES-->
+    <?php
+		$jsonFile = 'Services.json';
+		$services = readJSONFile($jsonFile);
+	?>
+    <!--START SERVICES-->
     <section class="section bg-light " id="services">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 offset-lg-2">
-                    <h1 class="section-title text-center">Our Services</h1>
+                    <h1 class="section-title text-center">Our Products and Services</h1>
                     <div class="section-title-border mt-3"></div>
-                    <p class="section-subtitle text-muted text-center pt-4 font-secondary">We craft digital, graphic and
-                        dimensional thinking, to create category leading brand experiences that have meaning and add a
-                        value for our clients.</p>
+                    <p class="section-subtitle text-muted text-center pt-4 font-secondary">We offer innovative virtual and 
+                    augmented reality solutions, including immersive educational experiences, thrilling adventures, advanced quantum computing 
+                    services, and online courses in cutting-edge technology.</p>
                 </div>
             </div>
             <div class="row mt-5">
+            <?php foreach ($services['keyProductsAndServices'] as $service) { ?>
                 <div class="col-lg-4 mt-4">
                     <div class="services-box">
                         <div class="d-flex">
                             <i class="pe-7s-diamond text-primary"></i>
                             <div class="ms-4">
-                                <h4>Digital Design</h4>
-                                <p class="pt-2 text-muted">Some quick example text to build on the card title and make
-                                    up the bulk of the card's content. Moltin gives platform.</p>
+                                <h4><?php echo $service['name']; ?></h4>
+                                <p class="pt-2 text-muted"><?php echo $service['description']; ?></p>
+                                <h4>Applications:</h4>
+                                <ul class="text-muted">
+                                    <?php foreach ($service['applications'] as $application) { ?>
+                                        <li>
+                                            <p><strong><?php echo $application['name']; ?>:</strong>
+                                            <?php echo $application['description']; ?></p>
+                                        </li>
+                                	<?php } ?>
+                            	</ul>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 mt-4">
-                    <div class="services-box">
-                        <div class="d-flex">
-                            <i class="pe-7s-display2 text-primary"></i>
-                            <div class="ms-4">
-                                <h4>Unlimited Colors</h4>
-                                <p class="pt-2 text-muted">Credibly brand standards compliant users without extensible
-                                    services. Anibh euismod tincidunt laoreet Ipsum passage.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mt-4">
-                    <div class="services-box">
-                        <div class="d-flex">
-                            <i class="pe-7s-piggy text-primary"></i>
-                            <div class="ms-4">
-                                <h4>Strategy Solutions</h4>
-                                <p class="pt-2 text-muted">Separated they live in Bookmarksgrove right at the coast of
-                                    the Semantics, and large language ocean neary regelia.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 mt-4">
-                    <div class="services-box">
-                        <div class="d-flex">
-                            <i class="pe-7s-science text-primary"></i>
-                            <div class="ms-4">
-                                <h4>Awesome Support</h4>
-                                <p class="pt-2 text-muted">It is a paradisematic country, in which roasted parts of
-                                    sentences fly into your mouth leave for the far World.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mt-4">
-                    <div class="services-box">
-                        <div class="d-flex">
-                            <i class="pe-7s-news-paper text-primary"></i>
-                            <div class="ms-4">
-                                <h4>Truly Multipurpose</h4>
-                                <p class="pt-2 text-muted">Even the all-powerful Pointing has no control about the blind
-                                    texts it is an almost unorthographic.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mt-4">
-                    <div class="services-box">
-                        <div class="d-flex">
-                            <i class="pe-7s-plane text-primary"></i>
-                            <div class="ms-4">
-                                <h4>Easy to customize</h4>
-                                <p class="pt-2 text-muted">Question Marks and devious Semikoli, but the Little Blind
-                                    Text didn’t listen. She packed her seven versalia.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-lg-4 mt-4">
-                    <div class="services-box">
-                        <div class="d-flex">
-                            <i class="pe-7s-arc text-primary"></i>
-                            <div class="ms-4">
-                                <h4>Pixel Perfect Design</h4>
-                                <p class="pt-2 text-muted">There are many variations of passages of Lorem Ipsum
-                                    available, but the majority have suffered alteration.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mt-4">
-                    <div class="services-box">
-                        <div class="d-flex">
-                            <i class="pe-7s-tools text-primary"></i>
-                            <div class="ms-4">
-                                <h4>Perfect Toolbox</h4>
-                                <p class="pt-2 text-muted">Hampden-Sydney College in Virginia, looked up one of the more
-                                    obscure Latin words, consectetur.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mt-4">
-                    <div class="services-box">
-                        <div class="d-flex">
-                            <i class="pe-7s-timer text-primary"></i>
-                            <div class="ms-4">
-                                <h4>Awesome Design</h4>
-                                <p class="pt-2 text-muted">All the Lorem Ipsum generators on the Internet tend to repeat
-                                    predefined chunks as necessary.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <?php } ?>
             </div>
         </div>
     </section>
-    <!--START SERVICES-->
+    <!--END SERVICES-->
     
     <!--SERVICES ABOVE-- JSON FILE -->
     
